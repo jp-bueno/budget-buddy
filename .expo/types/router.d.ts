5af1b31934b9adf5910348a042d112fda0e29ef4
joao.pedro <joaopedrobueno2910@gmail.com>
2024-08-28 21:52:43 -0300

    ajuste expo

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(authenticated)\(tabs)\_layout` | `/(authenticated)\(tabs)\crypto` | `/(authenticated)\(tabs)\home` | `/(authenticated)\(tabs)\invest` | `/(authenticated)\(tabs)\lifestyle` | `/(authenticated)\(tabs)\transfers` | `/_sitemap` | `/help` | `/login` | `/signup`;
+      StaticRoutes: `/` | `/(authenticated)` | `/(authenticated)/(tabs)` | `/(authenticated)/(tabs)/crypto` | `/(authenticated)/(tabs)/home` | `/(authenticated)/(tabs)/invest` | `/(authenticated)/(tabs)/lifestyle` | `/(authenticated)/(tabs)/transfers` | `/(authenticated)/crypto` | `/(authenticated)/home` | `/(authenticated)/invest` | `/(authenticated)/lifestyle` | `/(authenticated)/transfers` | `/(tabs)` | `/(tabs)/crypto` | `/(tabs)/home` | `/(tabs)/invest` | `/(tabs)/lifestyle` | `/(tabs)/transfers` | `/_sitemap` | `/crypto` | `/help` | `/home` | `/invest` | `/lifestyle` | `/login` | `/signup` | `/transfers`;
       DynamicRoutes: `/verify/${Router.SingleRoutePart<T>}`;
       DynamicRouteTemplate: `/verify/[phone]`;
     }
